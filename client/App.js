0921b1829860658ff21fabfd7478b7d023da5ef4
jiwansapkota <36408400+jiwansapkota@users.noreply.github.com>
2021-05-25 11:56:03 +0545

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,103 @@
+import { React, useState, useEffect } from 'react'
+import SearchBar from 'material-ui-search-bar'
+import BasicTable from './components/Table'
+import axios from 'axios'
+
+function App() {
+  const [showTable, setshowTable] = useState(false);
+  const [searchValue, setsearchValue] = useState("");
+  const [userDetails, setuserDetails] = useState("");
+  const [chooseDatabase, setchooseDatabase] = useState("npstock")
+  useEffect(() => {
+    axios.get("http://localhost:5000/user/details/npstock").then((response) => {
+      console.log("here are rows");
+      console.log(response.data.msg);
+      console.log(response.data.rows);
+      var tempData = JSON.parse(response.data.rows);
+      setuserDetails(tempData);
+      console.log(userDetails);
+      setshowTable(true);
+    })
+
+  }, [])
+
+  const fetchAllDataByOption = (option) => {
+    axios.get(`http://localhost:5000/user/details/${option}`).then((response) => {
+      console.log("here are rows");
+      console.log(response.data.msg);
+      console.log(response.data.rows);
+      var tempData = JSON.parse(response.data.rows);
+      setuserDetails(tempData);
+      console.log(userDetails);
+      setshowTable(true);
+    })
+  }
+
+  const onOptionChangeHandler = (event) => {
+    console.log("onchangeHandler called");
+    event.preventDefault();
+    console.log(event.target.value);
+    setchooseDatabase(event.target.value);
+    fetchAllDataByOption(event.target.value);
+
+  }
+  const searchHandler = () => {
+    console.log("enter pressed");
+      axios.post("http://localhost:5000/user/search", { "database": chooseDatabase, "value": searchValue }).then((response) => {
+        console.log("here are rows");
+        console.log(response.data.msg);
+        console.log(response.data.rows);
+        var tempData = JSON.parse(response.data.rows);
+        setuserDetails(tempData);
+        console.log(userDetails);
+        setshowTable(true);
+      })
+  }
+
+  // const handleKeyDown = (e) => {
+  //   console.log("enter pressed");
+  //   if (e.key === "Enter") {
+  //     console.log("condition true")
+  //     searchHandler();
+  //   }
+  // }
+
+  // const triggerChange = () => {
+  //   const { value } = this.state;
+  // }
+
+
+
+  const renderTable = () => {
+    if (showTable) {
+      return <BasicTable rows={userDetails} />
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <div className="searchBarWrapper" style={{
+        width: "80%", margin: "10px auto", position: "fixed",
+        top: 0, left: "10%", display: "inline"
+      }}>
+        <SearchBar
+          value={searchValue}
+          onChange={(newValue) => setsearchValue(newValue)}
+          onRequestSearch={ searchHandler}
+        />
+        <select value={chooseDatabase} onChange={onOptionChangeHandler}>
+          <option name="npstocks" value="npstock">npstock</option>
+          <option name="systemxlite" value="systemxlite">systemxlite</option>
+        </select>
+      </div>
+      <div className="tableWrapper" style={{ margin: "60px 10px 10px 10px", }}>
+        {renderTable()}
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App

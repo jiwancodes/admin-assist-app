f27237ab47e28ef0d739ebbb7b6c9e014db17b1e
jiwansapkota <jwnspkt@gmail.com>
2021-06-15 16:46:36 +0545

    server ready for production

--- client/db-admin-assist/src/axios-order.js
+++ client/db-admin-assist/src/axios-order.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 function getUrl(){
-    console.log("window._env_:",typeof window._env_);
+    console.log("window environment",typeof window._env_);
     const env_var = (typeof window._env_ !== "undefined")?window._env_:process.env;
-    console.log(env_var.REACT_APP_SERVER_URL);
     return env_var.REACT_APP_SERVER_URL;
 }
 const axiosOrder = axios.create({
--- server/index.js
+++ server/index.js
@@ -3,25 +3,25 @@ const cors = require('cors')
 const dotenv = require("dotenv").config();
 const actions = require('./methods/actions')
 const routes = require('./routes/route');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 // const passport = require('passport');
 const session = require('express-session');
 
 
 const app = express()
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-}
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'))
+// }
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    key: 'idlogin',
-    secret: process.env.ENCRYPTION_KEY,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 60 * 60 * 24,
-    }
-}))
+// app.use(session({
+//     key: 'idlogin',
+//     secret: process.env.ENCRYPTION_KEY,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         expires: 60 * 60 * 24,
+//     }
+// }))
 app.use(express.json())
 app.use(cors())
 app.use(express.static("."));
@@ -30,6 +30,6 @@ app.use(routes);
 // app.use(passport.initialize())
 // actions.passportStrategy(passport);
 
-const PORT = process.env.PORT || 6797;
+const PORT = process.env.PORT;
 // const PORT = 6797;
 app.listen(PORT, console.log(`server running in port ${PORT} in ${process.env.NODE_ENV} mode`))
